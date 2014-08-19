dbf51ac8971fcaadb62c5eb70b6c56d588415527
olaroos <ola.per.roos@gmail.com>
2014-08-19 14:54:22 +0200

    added select tiles with all unique column values for creation of new articles

--- homeBase.js
+++ homeBase.js
@@ -53,25 +53,35 @@ var angularApp = new angular.module('angularApp',['ui.unique'], function($httpPr
 angularApp.controller('angularController', function($scope, $http, $rootScope){
 	
 	$scope.haveSelected = false;
+	$scope.hideOn   	= false;
 	$scope.tableColumns = {'category' : 'category', 'description' : 'description', 'place' : 'place', 'relation' : 'relation', 
 										'title' : 'title', 'bestbefore' : 'bestbefore', 'borrowedfrom' : 'borrowedfrom',
 										'subinfo' : 'subinfo', 'author' : 'author', 'quantity' : 'quantity', 'id' : 'id' };
 
 	$http.post('./getData.php', {'category' :'notes'} ).success(function(data){
 		
-		$scope.chunks 	= data;		
-		$scope.keys   	= {};
-		$scope.out 		= {};
-		$scope.hideOn   = false;
-
-		var uniqueCategories = {};
+		$scope.chunks 			= data;		
+		$scope.keys   			= {};
+		$scope.out 				= {};
+		var uniqueCategories 	= {};
+		$scope.uniqueColumns 	= {};
 
 		for (var item in data) {
 			if (!uniqueCategories.hasOwnProperty(data[item]['category'])){
 				uniqueCategories[data[item]['category']] = data[item]['category'];
 			}
-		}		
-		
+		}					
+		/* sort out unique elements of database columns to aid creation of new articles */
+		for (var key in $scope.tableColumns) {			
+			$scope.uniqueColumns[key] = {};
+			console.log(key);
+			if ( (key !== "id") && (key !== "description")) {
+				for (var item in data) {
+					if ( (!$scope.uniqueColumns[key].hasOwnProperty(data[item][key])) && (data[item][key] !== "") && (data[item][key] !== " ") )
+						$scope.uniqueColumns[key][data[item][key]] = data[item][key];
+				}
+			}			
+		}
 		for (var item in uniqueCategories) {			
 			$scope.out[item] = [];
 			for (var dataItem in data) {
@@ -80,29 +90,15 @@ angularApp.controller('angularController', function($scope, $http, $rootScope){
 				}
 			}
 		}				
-		for (var item in data[0]){
+		for (var item in data[0]) {
 			if (item !== "$$hashKey")
 				$scope.keys[item] = item;
-			console.log(item);	
 		}			
 	});	
 
-	$scope.setSelectedTitle = function (value) {
-		/* reset color of selected article button */
-		if ($scope.haveSelected) {
-			document.getElementById('selectButton'+$scope.selected.id).style.color = 'black';
-		}
-		$scope.haveSelected = false;
 
-        if ($scope.selectedTitle === value) {
-            $scope.selectedTitle = undefined;
-        } else {
-            $scope.selectedTitle = value;
-        }
-	};
-	$scope.byTitle = function(entry){
-    	return entry.category === $scope.selectedTitle || $scope.selectedTitle === undefined;
-	};    
+	/* functions activated by buttons */
+
 	$scope.select = function(chunk, idx){
 		/* reset color of selected article button */
 		if ($scope.haveSelected) {
@@ -138,34 +134,6 @@ angularApp.controller('angularController', function($scope, $http, $rootScope){
 			$scope.hideOn = true;
 			$scope.hide($scope.selected);
 		} 			
-	}
-	$scope.unHide = function() {
-		console.log("unHiding");
-
-		for (item in $scope.keys) {
-			console.log(item);
-			document.getElementById(item).style.display = "inline";
-			document.getElementById(item+"Parent").style.display = "inline";
-		}
-	}
-	$scope.hide   = function(chunk){
-		console.log("hidefunction");
-		for (item in chunk) {
-			// console.log(item);
-			console.log(chunk[item]);
-			// console.log($scope.keys[item]);
-			if( ((chunk[item] == null) || (chunk[item] == '')) && (chunk[item] != '$$hashKey')) {
-				document.getElementById(item).style.display = "none";
-				document.getElementById(item+"Parent").style.display = "none";
-			}
-		}
-	}
-
-	$scope.showDeleteButton = function(){
-		if (document.getElementById("deleteButton").style.display == 'inline')
-			document.getElementById("deleteButton").style.display = 'none';
-		else 
-			document.getElementById("deleteButton").style.display = 'inline';
 	}
 	$scope.deleteData 		= function(){
 
@@ -257,10 +225,8 @@ angularApp.controller('angularController', function($scope, $http, $rootScope){
 			}
 		});
 	}
-	$scope.addData 		= function() {
-		console.log("adding data to database");
-		document.getElementById("informationDiv").innerHTML = "<center> added new article to database </center>";
-
+	$scope.addNewData 		= function() {
+		
 		var container 	= document.getElementById('addForm');
 		var child 		= container.firstChild;
 		var toWrite 	= {};
@@ -277,22 +243,82 @@ angularApp.controller('angularController', function($scope, $http, $rootScope){
 			child = child.nextSibling;				
 		}
 		
-		var category       = toWrite["categoryAdd"];
-		var description    = toWrite["descriptionAdd"];
-		var place          = toWrite["placeAdd"];
-		var relation       = toWrite["relationAdd"];
-		var title          = toWrite["titleAdd"];
-		var bestbefore     = toWrite["bestbeforeAdd"];
-		var borrowedfrom   = toWrite["borrowedfromAdd"];
-		var subinfo        = toWrite["subinfoAdd"];
-		var author         = toWrite["authorAdd"];
-		var quantity       = toWrite["quantityAdd"];
-		var id             = toWrite["idAdd"];
-
-		$http.post('./addnewData.php', { 'category' : category, 'description' : description, 'place' : place, 'relation' : relation, 'title' : title, 'bestbefore' : bestbefore, 'borrowedfrom' : borrowedfrom, 'subinfo' : subinfo, 'author' : author, 'quantity' : quantity, 'id' : id } ).success(function(data){
+		$http.post('./addnewData.php', {'category' 		: toWrite["categoryAdd"], 
+										'description' 	: toWrite["descriptionAdd"], 
+										'place' 		: toWrite["placeAdd"], 
+										'relation' 		: toWrite["relationAdd"], 
+										'title' 		: toWrite["titleAdd"], 
+										'bestbefore' 	: toWrite["bestbeforeAdd"], 
+										'borrowedfrom' 	: toWrite["borrowedfromAdd"], 
+										'subinfo' 		: toWrite["subinfoAdd"], 
+										'author' 		: toWrite["authorAdd"], 
+										'quantity' 		: toWrite["quantityAdd"], 
+										'id' 			: toWrite["idAdd"] 
+										}).success(function(data){
+											console.log("successfully sent add request to database");											
 											console.log(data);
+											document.getElementById("informationDiv").innerHTML = "<center> successfully sent add request to database </center>";
 										});
-	}						
+	}		
+	$scope.toggleUpdateAdd = function() {
+		if (document.getElementById("updateFormHolder").style.display == "none") {
+			document.getElementById("updateFormHolder").style.display = "inline";
+			document.getElementById("addDataFormHolder").style.display = "none";
+		} 
+		else {
+			document.getElementById("updateFormHolder").style.display = "none";
+			document.getElementById("addDataFormHolder").style.display = "inline";
+		}		
+	}
+
+	/* end functions activated by buttons */
+
+	/* help functions */
+
+	$scope.setSelectedTitle = function(value) {
+		/* reset color of selected article button */
+		if ($scope.haveSelected) {
+			document.getElementById('selectButton'+$scope.selected.id).style.color = 'black';
+		}
+		$scope.haveSelected = false;
+
+        if ($scope.selectedTitle === value) {
+            $scope.selectedTitle = undefined;
+        } else {
+            $scope.selectedTitle = value;
+        }
+	};
+	$scope.byTitle = function(entry){
+    	return entry.category === $scope.selectedTitle || $scope.selectedTitle === undefined;
+	};    	
+	$scope.unHide = function() {
+		console.log("unHiding");
+
+		for (item in $scope.keys) {
+			console.log(item);
+			document.getElementById(item).style.display = "inline";
+			document.getElementById(item+"Parent").style.display = "inline";
+		}
+	}
+	$scope.hide   = function(chunk){
+		console.log("hidefunction");
+		for (item in chunk) {
+			// console.log(item);
+			console.log(chunk[item]);
+			// console.log($scope.keys[item]);
+			if( ((chunk[item] == null) || (chunk[item] == '')) && (chunk[item] != '$$hashKey')) {
+				document.getElementById(item).style.display = "none";
+				document.getElementById(item+"Parent").style.display = "none";
+			}
+		}
+	}
+	$scope.showDeleteButton = function(){
+		if (document.getElementById("deleteButton").style.display == 'inline')
+			document.getElementById("deleteButton").style.display = 'none';
+		else 
+			document.getElementById("deleteButton").style.display = 'inline';
+	}
+					
 
 
 
